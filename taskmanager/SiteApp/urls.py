3efa29293f470b0e27945549bb452f23da68adb0
Derkachev1 <derkachev.04@mail.ru>
2024-11-28 00:20:49 +0300

    update and delete items

--- taskmanager/SiteApp/urls.py
+++ taskmanager/SiteApp/urls.py
@@ -4,9 +4,10 @@ from SiteApp import views
 app_name = "SiteApp"
 
 urlpatterns = [
-    path('', views.index),
-
+    path('', views.index, name="index"),
     path('<int:item_id>/', views.indexItem, name="detail"),
-
+    path('additem/', views.add_item, name="add_item"),
+    path('updateitem/<int:item_id>/', views.update_item, name="update_item"),
+    path('deleteitem/<int:item_id>/', views.delete_item, name="delete_item"),
     path('mainpage/', views.mainpage)
 ]
--- taskmanager/SiteApp/views.py
+++ taskmanager/SiteApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Product
 
@@ -15,6 +15,40 @@ def indexItem(request, item_id):
         "item":items
     }
     return render(request, "SiteApp/detail.html", context=context)
+
+def add_item(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        image = request.FILES["upload"]
+        item = Product(name=name, price=price, description=description, image=image)
+        item.save()
+    return render(request, "SiteApp/additem.html")
+
+def update_item(request, item_id):
+    item = Product.objects.get(id=item_id)
+    if request.method == "POST":
+        item.name = request.POST.get("name")
+        item.price = request.POST.get("price")
+        item.description = request.POST.get("description")
+        item.image = request.FILES.get("upload", item.image)
+        item.save()
+        return redirect("/SiteApp/")
+    context = {
+        "item":item
+    }
+    return render(request, "SiteApp/updateitem.html", context)
+
+def delete_item(request, item_id):
+    item = Product.objects.get(id=item_id)
+    if request.method == "POST":
+        item.delete()
+        return redirect("/SiteApp/")
+    context = {
+        "item":item
+    }
+    return render(request, "SiteApp/deleteitem.html", context)
 
 def mainpage(request):
     return HttpResponse("Главная страница")
