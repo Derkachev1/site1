98a5dc90f2d3f081bb0fbd861e08fdac02a2b4ec
Derkachev1 <derkachev.04@mail.ru>
2024-12-11 19:08:48 +0300

    search

--- taskmanager/SiteApp/urls.py
+++ taskmanager/SiteApp/urls.py
@@ -4,8 +4,8 @@ from SiteApp import views
 app_name = "SiteApp"
 
 urlpatterns = [
-    #path('', views.index, name="index"),
-    path('', views.ProductListView.as_view(), name="index"),
+    path('', views.index, name="index"),
+    #path('', views.ProductListView.as_view(), name="index"),
     #path('<int:item_id>/', views.indexItem, name="detail"),
     path('<int:pk>/', views.ProductDetailView.as_view(), name="detail"),
     path('additem/', views.add_item, name="add_item"),
--- taskmanager/SiteApp/views.py
+++ taskmanager/SiteApp/views.py
@@ -5,18 +5,28 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import DeleteView
 from django.urls import reverse_lazy
+from django.core.paginator import Paginator
 
-#def index(request):
- #   item = Product.objects.all()
- #   context = {
- #       "items":item
-  #  }
-  #  return render(request, "SiteApp/index.html", context)
+def index(request):
+    page_obj=item = Product.objects.all()
+
+    item_name = request.GET.get("search")
+    if item_name != '' and item_name is not None:
+        page_obj = item.filter(name__icontains=item_name)
+
+    paginator = Paginator(page_obj, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        "items":item, "page_obj":page_obj
+    }
+    return render(request, "SiteApp/index.html", context)
 
 class ProductListView(ListView):
     model = Product
     template_name = "SiteApp/index.html"
     context_object_name = "items"
+    paginate_by = 3
 
 #def indexItem(request, item_id):
  #   items = Product.objects.get(id=item_id)
